f5c99c621924bd5a522b0aa378970bc949e8b0ed
Oscar López <oscareduardolp6@gmail.com>
2024-06-26 19:31:06 +0000

    refactor: :coffin: Removed Dead Code

--- src/GasRecord/Infrastructure/BBoldGasRecordRepository/GetById.go
+++ src/GasRecord/Infrastructure/BBoldGasRecordRepository/GetById.go
@@ -42,53 +42,6 @@ func (repo *bboldGasRepository) GetById(id string) (GasRecord, error) {
 
 	return record, nil
 
-	// foundChan := make(chan GasRecord)
-	// errorChan := make(chan error)
-
-	// go func() {
-	// 	defer close(errorChan)
-	// 	transactionError := repo.db.View(func(transaction *bbolt.Tx) error {
-	// 		defer close(foundChan)
-	// 		bucket := transaction.Bucket([]byte(bucket_name))
-	// 		bucketNotFound := bucket == nil
-	// 		if bucketNotFound {
-	// 			return bucketNotFoundError()
-	// 		}
-	// 		byteRecord := bucket.Get([]byte(id))
-
-	// 		if byteRecord == nil {
-	// 			return RecordNotFound{id}
-	// 		}
-
-	// 		var rawRecord bboldGasRecord
-
-	// 		parsingError := json.Unmarshal(byteRecord, &rawRecord)
-
-	// 		if parsingError != nil {
-	// 			return newParsingRecordError(byteRecord)
-	// 		}
-
-	// 		record := fromBBoldGasRecord(rawRecord)
-
-	// 		foundChan <- record
-	// 		return nil
-	// 	})
-
-	// 	if transactionError != nil {
-	// 		errorChan <- transactionError
-	// 	}
-
-	// }()
-
-	// for {
-	// 	select {
-	// 	case foundRecord := <-foundChan:
-	// 		return foundRecord, nil
-	// 	case gettingError := <-errorChan:
-	// 		return GasRecord{}, gettingError
-	// 	}
-
-	// }
 }
 
 type RecordNotFound struct {
